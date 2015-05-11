6e8da0583b464f4141bc1ce31bcf2242fe43da3b
Alexander Willer <awi@alexander-willer.de>
2015-05-11 02:07:27 +0200

    Initial commit

---
+++ source/avr_timeslot-allocator.c
@@ -0,0 +1,17 @@
+/*
+ * avr_timeslot_allocator.c
+ *
+ * Created: 10.05.2015 02:27:48
+ *  Author: John Doe
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
